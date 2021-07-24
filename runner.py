52417b906656ea5e687330e09944eb1f087eac0c
Maxime <maxrichard@apple.com>
2021-07-23 19:15:28 -0700

    first commit

---
+++ runner.py
@@ -0,0 +1,158 @@
+import argparse
+import datetime
+import logging
+import os
+import sys
+
+import pandas as pd
+import requests
+import time
+import yaml
+import zipfile
+
+from discord_webhook import DiscordWebhook
+from pathlib import Path
+
+from utils import (
+    SQLiteExecutor,
+    logging_map,
+    create_log_dir,
+    flatten_list,
+    paths
+)
+
+FINANCIAL_DISCLOSURE_HOME = "https://disclosures-clerk.house.gov/PublicDisclosure/FinancialDisclosure"
+FINANCIAL_DISCLOSURE_REPORT_URL = 'https://disclosures-clerk.house.gov/public_disc/financial-pdfs/{year}FD.ZIP'
+PERIODIC_TRANSACTION_REPORT_URL = 'https://disclosures-clerk.house.gov/public_disc/ptr-pdfs/{year}/{doc_id}.pdf'
+
+WEBHOOK_URL = "https://discord.com/api/webhooks/867903600591896603/4YSO2qg-tjAE4SucfpwEV0Xnb9auCIYmmj17ZdSpPgLGZlRiE54dbskGoKNzZ1HbI4eP"
+
+
+def post_message_discord(config, message):
+    logger.info(message)
+    webhook = DiscordWebhook(
+        url=config['url'],
+        username="RepresentativesAlert",
+        content=message
+    )
+    webhook.execute()
+
+
+def doc_id_exists(doc_id, doc_ids):
+    if doc_id in doc_ids:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    # Setup argument parser
+    parser = argparse.ArgumentParser('Gather configs for fetching ticker data')
+    parser.add_argument('-log-level', '--log-level',
+                        help='Set the level for logging',
+                        choices=('DEBUG', 'INFO', 'WARNING', 'ERROR'),
+                        default='INFO')
+
+    parser.add_argument("-e", "--env",
+                        help="Environement the code is running on",
+                        type=str, default='local')
+
+    args = parser.parse_args()
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging_map[args.log_level])
+
+    log_directory, log_file_name = create_log_dir('HORScrapper', args.env)
+    log_format = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s",datefmt="%H:%M:%S")
+
+    file_handler = logging.FileHandler(log_directory + "/" + log_file_name)
+    file_handler.setLevel(logging_map[args.log_level])
+    file_handler.setFormatter(log_format)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(log_format)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    day = datetime.datetime.now()
+    year = day.strftime("%Y")
+
+    with open('configs/config.yml') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    # DOWNLOAD YEALRY ZIP FILE
+    r = requests.get(FINANCIAL_DISCLOSURE_REPORT_URL.format(
+        year=year
+    ))
+
+    # Save zip file
+    zipfilename = 'data/fdrs_zip/{}.zip'.format(
+        year
+    )
+    with open(zipfilename, 'wb') as f:
+        f.write(r.content)
+
+    # Extract zip file
+    with zipfile.ZipFile(zipfilename) as z:
+        z.extractall('./data/fdrs')
+
+    # Open content of fdr file
+    filename = 'data/fdrs/{}FD.txt'.format(
+        year
+    )
+    fdrs = pd.read_csv(filename, sep='\t')
+
+    # SQLiteExecutor object
+    sql_executor = SQLiteExecutor(
+        env=args.env, logger=logger,
+        path_to_db='data/sql/fdr.db'
+    )
+    # Get DocID from DB
+    doc_ids = flatten_list(
+        sql_executor.execute_query("select DocID from fdr")
+    )
+
+    if not doc_ids:
+        message = "No new trades from representatives"
+        logger.info(message)
+        post_message_discord(config['discord'], message)
+    else:
+        message = "New trades from representatives!\n{home_url}".format(
+            home_url=FINANCIAL_DISCLOSURE_HOME
+        )
+        logger.info(message)
+        post_message_discord(config['discord'], message)
+
+        # Only keep new DocIDs
+        fdrs['NewID'] = fdrs.apply(lambda x: doc_id_exists(x.DocID, doc_ids), axis=1)
+        new_fdrs = fdrs[fdrs['NewID'] == False].drop(['NewID'], axis = 1)
+
+        for idx, row in new_fdrs.iterrows():
+            name = "{first} {last}".format(
+                first=row['First'],
+                last=row['Last']
+            )
+            url = PERIODIC_TRANSACTION_REPORT_URL.format(
+                year=year, doc_id=row['DocID']
+            )
+            message = "{name} traded! {docid}: {url}".format(
+                name=name,
+                docid=row['DocID'],
+                url=url
+                )
+            logger.info(message)
+
+            post_message_discord(config['discord'], message)
+            time.sleep(3)
+
+        # Save content of file in sqlite fdr table
+        sql_executor.save_pandas_df(new_fdrs, table='fdr')
+
+
+    # Delete content from dirs
+    path = Path("{}/data/".format(paths[args.env]))
+    for dir in path.iterdir():
+        if dir.__str__().split('/')[-1] != "sql":
+            sub_path = Path(dir.__str__())
+            for file in sub_path.iterdir():
+                logger.info("Removing {}".format(file))
+                os.remove(file)
---
+++ utils.py
@@ -0,0 +1,63 @@
+import datetime
+import errno
+import logging
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+
+paths = {
+    "local": "/Users/maximerichard/dev/nancy",
+    "raspberry": "/home/pi/wsbscraper"
+}
+
+logging_map = {
+        'DEBUG': logging.DEBUG,
+        'INFO': logging.INFO,
+        'WARNING': logging.WARNING,
+        'ERROR': logging.ERROR
+    }
+
+def create_log_dir(name, env):
+    date = datetime.datetime.now()
+    log_directory = date.strftime('{}/logs/%Y_%m_%d'.format(
+        paths[env]
+    ))
+    try:
+        os.makedirs(log_directory)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+    log_file_name = date.strftime('{}_%d_%m_%Y_%H_%M.log'.format(name))
+    return log_directory, log_file_name
+
+def flatten_list(a_list):
+    return [element[0] for element in a_list]
+
+class SQLiteExecutor(object):
+    """Class for executing SQL statements"""
+    def __init__(self, env, logger, path_to_db):
+        self.env = env
+        self.logger = logger
+        self.path_to_db = path_to_db
+
+    @property
+    def engine(self, echo=False):
+        path_to_db = "{env}/{path_to_db}".format(
+            env=paths[self.env],path_to_db=self.path_to_db)
+        self.logger.info("[SQLiteExecutor] Path to db: {}".format(path_to_db))
+        sqlite_loc = 'sqlite:///{path}'.format(path=path_to_db)
+        engine = create_engine(sqlite_loc, echo=echo)
+        return engine
+
+    def save_pandas_df(self, df, table, if_exists='append'):
+        self.logger.info("[SQLiteExecutor] Saving pandasdf to table: {}".format(
+            table))
+        df.to_sql(table, self.engine, if_exists=if_exists, index=False)
+
+    def execute_query(self, query):
+        with self.engine.begin() as con:
+            return con.execute(query).fetchall()
+
+    def insert_query(self, query):
+        with self.engine.begin() as con:
+            return con.execute(query)
